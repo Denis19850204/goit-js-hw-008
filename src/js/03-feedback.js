fb8d65be0434e6b9e6a04d37c68e75dead88b70a
Denis19850204 <dg04021985@gmail.com>
2022-09-03 23:30:50 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 
 const formFeedback = document.querySelector('.feedback-form');
 
-getMessageFromLocalStorage()
+getMessageFromLocalStorage();
 
 formFeedback.addEventListener('input', throttle(feedbackMessage, 500));
 formFeedback.addEventListener('submit', sendForm);
@@ -10,27 +10,25 @@ formFeedback.addEventListener('submit', sendForm);
 const formData = {};
 
 function feedbackMessage(e) {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
- 
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
 function sendForm(e) {
-    e.preventDefault();
-    e.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');   
-    console.log(formData)
-
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(formData);
 }
 
 function getMessageFromLocalStorage() {
-    const savedMessage = localStorage.getItem('feedback-form-state');
-    const parsedSavedMessage = JSON.parse(savedMessage);
-    
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  const parsedSavedMessage = JSON.parse(savedMessage);
+  const formEmail = formFeedback[0];
+  const formMessage = formFeedback[1];
 
-    if (parsedSavedMessage) {
-        formFeedback[0].value = parsedSavedMessage.email;
-        formFeedback[1].value = parsedSavedMessage.message;
-    }    
-   
+  if (parsedSavedMessage) {
+    formEmail.value = parsedSavedMessage.email || '';
+    formMessage.value = parsedSavedMessage.message || '';
+  }
 }
